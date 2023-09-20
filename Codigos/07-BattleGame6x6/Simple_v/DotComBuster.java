8e858f6dddb8541b788242e567dd6477d45393ad
NatanSan2002 <natansantosgomes@gmail.com>
2023-09-20 17:38:28 -0300

    Versão battleGame 6x6

---
+++ Codigos/07-BattleGame6x6/Simple_v/DotComBuster.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class DotComBuster{
+
+     //GameHelper helper;
+
+    ArrayList<DotCom> dotComList;
+
+    int numGuess; 
+    public static void main(String[] args){
+    
+    }
+
+
+ public void SetUpGame(){
+      dotComList.add(new DotCom());
+      dotComList.add(new DotCom());
+      dotComList.add(new DotCom());
+
+    System.out.println("Barcos foram criados!");
+     System.out.println(dotComList.get(1));
+    
+    }
+
+
+
+
+}
